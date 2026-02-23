b16cd3cd8653db267257d484a64a64c02c1386b1
Mostafa Mahmoud Salah <62766443+Mostafamahmoud12824@users.noreply.github.com>
2026-02-23 15:36:16 +0200

    Add files via upload

---
+++ استعلام عن كود الصنف والاسم والسعر.sql
@@ -0,0 +1,38 @@
+﻿WITH Items_CTE AS (
+    SELECT
+        p.ID AS ItemID,   -- رقم الصنف المسجل في السيستم
+
+        -- الاسم العربي
+        LTRIM(RTRIM(p.ItemName)) AS ItemName_AR,
+
+        -- الاسم الإنجليزي (ولو مش موجود يرجع العربي)
+        LTRIM(RTRIM(ISNULL(m.itname_en, p.ItemName))) AS ItemName_EN,
+
+        pr.itprice AS Price,
+        p.Style_Code,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+                LTRIM(RTRIM(ISNULL(m.itname_en, p.ItemName)))
+            ORDER BY p.ID
+        ) AS rn
+    FROM TblProductItem p
+    INNER JOIN prices_items pr
+        ON p.ID = pr.itid
+    LEFT JOIN select_sub_men_items m
+        ON LTRIM(RTRIM(p.ItemName)) = LTRIM(RTRIM(m.itname))
+    WHERE
+        p.ItemName IS NOT NULL
+        AND LTRIM(RTRIM(p.ItemName)) <> ''
+        AND pr.itprice > 0
+)
+
+SELECT
+    ItemID,        -- رقم الصنف
+    ItemName_AR,
+    ItemName_EN,
+    Price,
+    Style_Code
+FROM Items_CTE
+WHERE rn = 1
+ORDER BY ItemID ASC;   -- 🔹 من الأصغر للأكبر
